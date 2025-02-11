f2b565691129997ed492b6c17396afb058f7d57f
Tom Bamford <tom@annertech.com>
2025-02-11 16:28:46 +0000

    fix: history default to route path, ignoring pathname

--- src/components/color_form/color_form.js
+++ src/components/color_form/color_form.js
@@ -246,7 +246,8 @@ EightShapes.ColorForm = (function () {
   }
 
   function updateUrl() {
-    window.history.pushState(false, false, "/?" + $colorForm.serialize());
+    const url = window.location.origin + window.location.pathname + "?" + $colorForm.serialize();
+    window.history.pushState(false, false, url);
   }
 
   // function disableFormFields() {
